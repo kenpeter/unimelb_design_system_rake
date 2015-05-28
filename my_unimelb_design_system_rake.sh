a72fe35f164bf637ec6aed71e55c3e4c1e2ac8ca
kenpeter <figo2476@hotmail.com>
2015-05-28 17:14:14 +1000

    Apply patch to header 1.0 scss

--- my_unimelb_design_system_rake.sh
+++ my_unimelb_design_system_rake.sh
@@ -3,10 +3,13 @@
 # unimelb-design-system is checked out from https://github.com/marcom-unimelb/unimelb-design-system
 
 UOM_DESIGN_SYSTEM_DIR="unimelb-design-system"
-MY_SCRIPT_DIR="my_rake"
-MY_DIFF_FILE="unimelb_design_system_1.0_rakefile.diff"
+MY_SCRIPT_DIR="unimelb_design_system_rake"
+MY_RAKE_DIFF_FILE="unimelb_design_system_1.0_rakefile.diff"
+MY_HEADER_SCSS_DIFF_FILE="unimelb_design_system_header_1.0_scss.diff"
+UOM_DESIGN_SYSTEM_HEADER_DIR="injection/header/"
 
-# cd
+
+# In uom design system dir and stay
 cd ../$UOM_DESIGN_SYSTEM_DIR 
 
 # Clean up
@@ -19,15 +22,22 @@ rm ./build -rf
 # The 1.0 build uses ruby 2.2.1
 git checkout 2db206f5f94cdc4d02c822f7a62fcd60a50a46f9
 
-# Copy
-cp ../$MY_SCRIPT_DIR/$MY_DIFF_FILE .
-patch -p1 < $MY_DIFF_FILE 
+# Patch rake file 
+cp ../$MY_SCRIPT_DIR/$MY_RAKE_DIFF_FILE .
+patch -p1 < $MY_RAKE_DIFF_FILE 
 
+# Patch header scss
+cp ../$MY_SCRIPT_DIR/$MY_HEADER_SCSS_DIFF_FILE $UOM_DESIGN_SYSTEM_HEADER_DIR
+patch -p1 < $UOM_DESIGN_SYSTEM_HEADER_DIR/$MY_HEADER_SCSS_DIFF_FILE
+
+# Build
 rvm 2.2.1 exec bundle exec rake assets:compile VERSION=1.0
 
 # Self clean up
 git checkout Rakefile
-rm $MY_DIFF_FILE 
+git checkout $UOM_DESIGN_SYSTEM_HEADER_DIR/_header.scss 
+rm $MY_RAKE_DIFF_FILE 
+rm $UOM_DESIGN_SYSTEM_HEADER_DIR/$MY_HEADER_SCSS_DIFF_FILE
 
 
 # Build master
